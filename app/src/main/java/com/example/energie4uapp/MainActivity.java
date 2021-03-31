6b2f0001095aa6206c3590cc5d091e6c391edf74
Vlad <vlad210694@yahoo.com>
2021-03-31 15:47:03 +0200

    added redirect

--- app/src/main/java/com/example/energie4uapp/MainActivity.java
+++ app/src/main/java/com/example/energie4uapp/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -25,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, AddNewDefectActivity.class));
             }
         });
     }
